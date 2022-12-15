c0d278934eb36f459da350c8741ef61f39be13be
ernisarahmaa <ernisarahma@gmail.com>
2022-12-15 18:38:09 +0700

    add all files

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "YOURDEPLOYURL" // TODO: replace this
+const studentName = "ERNISA RAHMA WAHYUNI" // TODO: replace this
+const studentId = "FE2882471" // TODO: replace this
+
+export { url, studentName, studentId }
